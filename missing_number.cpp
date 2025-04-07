4ea4a582e8660d020c1a428229f4dcab210a5ea9
alain lim <alain.lim@edu.esiee.fr>
2025-04-07 11:56:47 +0200

    Improved solution

--- missing_number.cpp
+++ missing_number.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+#define ll long long 
+
+
 int main(){
-    int res{0};
-    int n;
+    ll res;
+    ll n;
     cin >> n;
- 
-    vector<int> nums(n-1);
-    for (int i = 0; i < n-1; i++){
-        cin >> nums[i];
+    
+    res = ((n+1) * n) / 2;
+
+    for (int i = 0; i < n - 1; i++){
+        int m;
+        cin >> m;
+        res -= m;
     }
- 
-    sort(nums.begin(), nums.end());
- 
-    for (int i = 0; i < n; i++){
-        if (i+1 != nums[i]){
-            res = i+1;
-            break;
-        }
-    }
- 
+
     cout << res;
  
     return 0;
