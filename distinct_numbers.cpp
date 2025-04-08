2ead97d4cb75362761af26c0c8a013b7f01f4b70
alain lim <alain.lim@edu.esiee.fr>
2025-04-08 19:37:18 +0200

    solved Distinct Numbers from Sorting and Searching

---
+++ distinct_numbers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    int n;
+    cin >> n;
+
+    set<int> s;
+
+    for (int i=0; i<n; i++){
+        int a;
+        cin >> a;
+
+        s.insert(a);
+    }
+
+    cout << s.size();
+
+    return 0;
+}
